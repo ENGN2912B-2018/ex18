127fe8887ac9974d1be60523a8d05599d41cdad9
Jason Gaudette <jason_gaudette@brown.edu>
2016-06-03 19:57:09 -0400

    initial import of code from Rolf

---
+++ parse.h
@@ -0,0 +1,13 @@
+// ParseParam.h -- See ParseParam.c for example of use
+
+#ifndef _PARSE_PARAM
+#define _PARSE_PARAM
+
+#include <stdlib.h>
+#include <iso646.h>
+
+extern char* read_par(const char* FileName, const char *VariableName);
+
+extern char *TempPP;
+
+#endif
